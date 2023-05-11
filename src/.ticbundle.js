dd06b76dc899141683d9bb7b5f4570d5322835a3
Boden McHale <boden.mchale@gmail.com>
2023-05-11 15:22:15 -0400

    Updates

---
+++ src/.ticbundle.js
@@ -0,0 +1,20 @@
+{
+  root: 'src',
+  wait: 200,
+  metadata: {
+    title: suqut,
+    author: LegoDinoMan,
+    desc: null,
+    script: 'lua',
+    input: null,
+    saveid: null
+  },
+  output: {
+    path: './builds',
+    extension: 'lua',
+    name: 'suqut'
+  },
+  files: [],
+  assets: [],
+  after: null
+}
